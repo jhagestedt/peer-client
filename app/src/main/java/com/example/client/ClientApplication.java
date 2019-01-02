2521405cbc34eea50f420878ec4056db5ff6c183
julien.hagestedt <julien.hagestedt@gmail.com>
2019-01-02 18:47:39 +0100

    generalize annotiation

--- app/src/main/java/com/example/client/ClientApplication.java
+++ app/src/main/java/com/example/client/ClientApplication.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
@@ -13,8 +13,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @EnableScheduling
-@EnableEurekaClient
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ClientApplication {
 
